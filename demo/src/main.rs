ad41e702f2186e9e6c1f6c79b123b3db4f53c2a7
Антон Фролов <F-Miralius@yandex.ru>
2024-08-05 16:56:23 +0400

    Добавлен checkbox для активации `reverse`

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -5,7 +5,7 @@ slint::include_modules!();
 fn main() {
     let mut audio_helper = audio::get_audio_helper();
     let main_window = MainWindow::new().unwrap();
-    main_window.on_play_audio_file(move || {
+    main_window.on_play_audio_file(move |checked| {
         audio_helper.play_file("audio/lyutyy-negr-oret.mp3");
     });
     main_window.run().unwrap();
