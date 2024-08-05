0a0492a784c755704393985e833c96de6df60147
Антон Фролов <F-Miralius@yandex.ru>
2024-08-05 16:37:31 +0400

    Связано воспроизведение (лютого негра) с кнопкой

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,6 +4,9 @@ slint::include_modules!();
 
 fn main() {
     let mut audio_helper = audio::get_audio_helper();
-    audio_helper.play_file("audio/lyutyy-negr-oret.mp3");
-    MainWindow::new().unwrap().run().unwrap();
+    let main_window = MainWindow::new().unwrap();
+    main_window.on_play_audio_file(move || {
+        audio_helper.play_file("audio/lyutyy-negr-oret.mp3");
+    });
+    main_window.run().unwrap();
 }
